c4ffaefbcc614053b04c09c993f2d84bffa932ce
Nadina <nadinalehtonen@kamk.fi>
2024-10-02 19:32:22 +0300

    dummy

---
+++ App.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config') // Tuo config, joka sisältää MONGODB_URI:n
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+// Yhdistä MongoDB:hen
+mongoose.connect(config.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
